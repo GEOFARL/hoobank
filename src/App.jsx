f6aca99c0e0642b8da06685093fd5b24a27e11d2
Maxim <toper.one111@gmail.com>
2023-07-25 22:23:10 +0300

    Markup main structure of a website

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,23 @@
+import styles from './styles.js';
+
 function App() {
-  return <h1>Hello world</h1>;
+  return (
+    <div className="w-full bg-primary overflow-hidden text-white">
+      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>Navbar</div>
+      </div>
+
+      <div className={`bg-primary ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>Hero</div>
+      </div>
+
+      <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          Stats Business Billing CardDeal Testimonials Clients CTA Footer
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
